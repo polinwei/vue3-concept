e99c8d80ae644028a9097903b0e135384f036714
polin.wei <polin.wei@gmail.com>
2021-10-04 17:31:57 +0800

    List Rendering

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       image: './assets/images/socks_green.jpg',
-      inventory: 100,
-      onSale: true
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      // solution
+      sizes: ['S', 'M', 'L', 'XL'],
+      // solution
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' },
+      ]
     }
   }
 })
