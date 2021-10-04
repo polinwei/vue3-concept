6cf25306a04808667d9257dbe0574d3c71d32744
polin.wei <polin.wei@gmail.com>
2021-10-04 16:49:46 +0800

    Conditional Rendering

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       image: './assets/images/socks_green.jpg',
-      url: 'https://www.vuemastery.com/'
+      inventory: 100,
+      onSale: true
     }
   }
 })
